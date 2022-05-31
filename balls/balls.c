986877f6d28a79242bd719a129be1739319c4b2f
pyrex8 <ev64bug@yahoo.com>
2022-05-30 19:27:39 -0700

    vline() uses eor for pixels

--- balls/balls.c
+++ balls/balls.c
@@ -232,7 +232,7 @@ static void hline(uint8_t column, uint8_t row, uint8_t length, uint8_t pixels)
 
 static void vline(uint8_t column, uint8_t row, uint8_t length, uint8_t pixels)
 {
-    // Assembly = 6700us, C = 9400us
+    // 11.2ms
     DATA1_P = pixels;
     DATA2_P = column;
     DATA3_P = row;
@@ -242,16 +242,17 @@ static void vline(uint8_t column, uint8_t row, uint8_t length, uint8_t pixels)
     __asm__ ("ldx %b", DATA4);          // Start at second-to-last row
 
     // loop
-    __asm__ ("vl1: txa");               // row to a
+    __asm__ ("vl1: dex");
+    __asm__ ("txa");               // row to a
     __asm__ ("tay");                    // row to y
     __asm__ ("lda (%b),y", LKLO);       // Get the row address
     __asm__ ("sta %b", ADDR1L);
     __asm__ ("lda (%b),y", LKHI);
     __asm__ ("sta %b", ADDR1H);
-    __asm__ ("lda %b", DATA1);
     __asm__ ("ldy %b", DATA2);          // column
+    __asm__ ("lda (%b),y", ADDR1L);
+    __asm__ ("eor %b", DATA1);
     __asm__ ("sta (%b),y", ADDR1L);
-    __asm__ ("dex");
     __asm__ ("cpx %b", DATA3);
     __asm__ ("bne vl1");
 }
@@ -436,8 +437,8 @@ static void hbox(void)
 {
     hline(COLUMN_FIRST, ROW_FIRST, COLUMNS, WHITE);
     hline(COLUMN_FIRST, ROW_LAST, COLUMNS, WHITE);
-    vline(COLUMN_FIRST, 0, 190, 0x03);
-    vline(COLUMN_LAST, 0, 190, 0x60);
+    vline(COLUMN_FIRST, 0, 192, 0x03);
+    vline(COLUMN_LAST, 0, 192, 0x60);
 }
 
 void delay(void)
@@ -514,7 +515,7 @@ void main(void)
 
         hline(5, 5, 20, WHITE);
         TEST_PIN_TOGGLE; // adds 2.5us
-        vline(10, 0, 190, 0x03);
+        vline(10, 0, 192, 0x03);
         TEST_PIN_TOGGLE; // adds 2.5us
     }
 }
